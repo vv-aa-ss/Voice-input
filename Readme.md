dc161d39f492c2f6bf81456aca737b637454fb27
Artsiom Vaschilo <v2322048@gmail.com>
2023-04-28 19:45:26 +0300

    init

---
+++ Readme.md
@@ -0,0 +1,4 @@
+<h2 align="center">Hi there, I'm <a href="https://www.linkedin.com/in/artem-vaschcilo-67ba08250/" target="_blank">Artsiom Vaschilo</a> 
+<img src="https://github.com/blackcater/blackcater/raw/main/images/Hi.gif" height="25"/></h1>
+<h3 align="center">In this script, I wanted to implement voice input of commands. The Russian recognition language is used.<p>
+
---
+++ main.py
@@ -0,0 +1,35 @@
+import speech_recognition
+
+sr = speech_recognition.Recognizer()
+sr.pause_threshold = 0.5
+
+
+def create_task():
+    text = ""
+    print("Что добавить в заметки?\n")
+    with open("to_do_list.txt", 'a') as file:
+        while text != "":
+            text = recognize_voice()
+            if text != "":
+                file.write(f'{recognize_voice()}')
+
+
+def recognize_voice():
+    with speech_recognition.Microphone() as mic:
+        sr.adjust_for_ambient_noise(source=mic, duration=0.5)
+        audio = sr.listen(source=mic)
+        query = sr.recognize_google(audio_data=audio, language="ru-RU").lower()
+    return query
+
+
+def main():
+    while True:
+        text = recognize_voice()
+        if text == 'добавить заметку':
+            create_task()
+        if text == 'выйти':
+            exit()
+        print(text)
+
+
+main()
